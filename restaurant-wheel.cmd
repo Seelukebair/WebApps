8a26f5210569603314c7896b35ba96ddb7de00c0
Seelukebair <63706646+Seelukebair@users.noreply.github.com>
2026-05-13 14:25:44 -0800

    Delete restaurant-wheel.cmd

--- restaurant-wheel.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Double-click to launch the Restaurant Wheel.
-REM Starts a local server (needed because browsers block fetch from file:// origins)
-REM and opens your default browser to the page.
-
-cd /d "%~dp0"
-start "" "http://localhost:8765/restaurant-wheel.html"
-node serve.js
